fc94006f42d9c37dfc521315a12a979e3e6b2d75
apresas- <albertprpu97@gmail.com>
2024-04-25 14:08:53 +0200

    Fixes in Makefiles + CPP_01,ex01 fixes

--- CPP_Module_01/ex01/main.cpp
+++ CPP_Module_01/ex01/main.cpp
@@ -1,24 +1,20 @@
 #include "Zombie.hpp"
 
-Zombie	*zombieHorde( int N, std::string name )
-{
-	Zombie *horde = new Zombie[N];
-
-	for (int i = 0; i < N; i++)
-		horde[i].setName(name);
-	
-	return (horde);
-}
+Zombie	*zombieHorde( int N, std::string name );
 
 int	main(void)
 {
 	int count = 5;
 
+	std::cout << "// Creating a horde of " << count << " zombies named Carlos:" << std::endl;
 	Zombie	*horde = zombieHorde(count, "Carlos");
 
+	std::cout << "// Announcing the horde:" << std::endl;
 	for (int i = 0; i < count; i++)
 		horde[i].announce();
 
+	std::cout << "// Deleting the horde:" << std::endl;
 	delete [] (horde);
+
 	return (0);
 }
