368e9978f81c521da48bc9a6ae1580134d413388
apresas- <albertprpu97@gmail.com>
2024-04-30 11:04:03 +0200

    CPP_Module_01, ex00: Minute fixes

--- CPP_Module_01/ex00/Zombie.cpp
+++ CPP_Module_01/ex00/Zombie.cpp
@@ -3,12 +3,12 @@
 Zombie::Zombie( std::string name )
 {
 	this->name = name;
-	std::cout << this->name << " was created" << std::endl;
+	std::cout << "Zombie " << this->name << " was created" << std::endl;
 }
 
 Zombie::~Zombie( void )
 {
-	std::cout << this->name << " was destroyed" << std::endl;
+	std::cout << "Zombie " << this->name << " was destroyed" << std::endl;
 }
 
 void	Zombie::announce( void ) const
