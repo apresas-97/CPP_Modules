2d3a85d89c422be49cf5b6aa69394c1afad207aa
apresas- <albertprpu97@gmail.com>
2024-05-09 15:30:21 +0200

    CPP_Module_03, ex01, modified ScavTrap class and main.cpp

--- CPP_Module_03/ex01/main.cpp
+++ CPP_Module_03/ex01/main.cpp
@@ -3,18 +3,35 @@
 
 int	main( void )
 {
-	ScavTrap a("Cachibache");
+	std::cout << "Creating ScavTrap:" << std::endl;
 
+	ScavTrap a("Cachibache");
 	a.attack("Monster");
 	a.takeDamage(25);
 	a.beRepaired(25);
 	a.guardGate();
 
 	std::cout << "--------" << std::endl;
+	std::cout << "Testing ScavTrap Copy constructor:" << std::endl;
+
+	ScavTrap c(a);
+	c.attack("Monster");
+	c.beRepaired(25);
+
+	std::cout << "--------" << std::endl;
+	std::cout << "Checing ScavTrap behavior when out of hit points:" << std::endl;
 
 	a.takeDamage(100);
 	a.beRepaired(100);
 	a.guardGate();
 
+	std::cout << "--------" << std::endl;
+	std::cout << "Creating ClapTrap:" << std::endl;
+
+	ClapTrap b("Bachibache");
+	b.attack("Monster");
+	b.takeDamage(10);
+
+	std::cout << "--------" << std::endl;
 	return 0;
 }
