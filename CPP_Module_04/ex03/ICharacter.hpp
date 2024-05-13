8aaff32a34bb7fc529eaf1e309835c5ba24d9504
apresas- <albertprpu97@gmail.com>
2024-05-13 18:41:44 +0200

    CPP_Module_04, ex03: ICharacter.hpp polish

--- CPP_Module_04/ex03/ICharacter.hpp
+++ CPP_Module_04/ex03/ICharacter.hpp
@@ -7,23 +7,13 @@ class AMateria;
 
 class ICharacter
 {
-
-protected:
-
-	// ICharacter( void );
-	// ICharacter( ICharacter const & src );
-	// ICharacter & operator=( ICharacter const & other );
-
-public:
-
-	virtual ~ICharacter( void ) {}
-
-	virtual std::string const &	getName( void ) const = 0;
-	virtual void				equip( AMateria* m ) = 0;
-	virtual void				unequip( int idx ) = 0;
-	virtual void				use( int idx, ICharacter & target ) = 0;
-	virtual AMateria*			getDroppedMaterias( void ) = 0;
-
+	public:
+		virtual ~ICharacter( void ) {}
+		virtual std::string const &	getName( void ) const = 0;
+		virtual void				equip( AMateria* m ) = 0;
+		virtual void				unequip( int idx ) = 0;
+		virtual void				use( int idx, ICharacter & target ) = 0;
+		virtual AMateria*			getDroppedMaterias( void ) = 0;
 };
 
 #endif // ICHARACTER_HPP
