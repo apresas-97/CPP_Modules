8ecb1f760d8815efe0e856311a5ee9dc9f5b7199
Albert <albertprpu97@gmail.com>
2024-09-25 14:52:28 +0200

    CPP_Module_07/ex00: Changed max function so it checks a > b, not a >= b, that's what the subject asks

--- CPP_Module_07/ex00/whatever.hpp
+++ CPP_Module_07/ex00/whatever.hpp
@@ -21,7 +21,7 @@ T const &	min(T const & a, T const & b )
 template<typename T>
 T const &	max( T const & a, T const & b )
 {
-	return ( a >= b ? a : b );
+	return ( a > b ? a : b );
 }
 
 #endif // WHATEVER_HPP
