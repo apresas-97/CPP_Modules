1e77e326acb03bf7c284e520fc40984a6e4a5338
Albert <albertprpu97@gmail.com>
2024-09-25 15:36:17 +0200

    CPP_Module_07/ex00: Forgot to add the test for min when a==b

--- CPP_Module_07/ex00/main.cpp
+++ CPP_Module_07/ex00/main.cpp
@@ -7,11 +7,11 @@ int	main( void )
 	int intB = 42;
 
 	std::cout <<"Comparing ints " << intA << " and " << intB << std::endl;
-	std::cout << "Max: " << max<int>(intA, intB) << std::endl;
-	std::cout << "Min: " << min<int>(intA, intB) << std::endl;
+	std::cout << "Max: " << ::max<int>(intA, intB) << std::endl;
+	std::cout << "Min: " << ::min<int>(intA, intB) << std::endl;
 	std::cout << "Performing swap" << std::endl;
 	std::cout << "Old values: " << intA << ", " << intB << std::endl;
-	swap<int>(intA, intB);
+	::swap<int>(intA, intB);
 	std::cout << "New values: " << intA << ", " << intB << std::endl;
 	std::cout << std::endl;
 
@@ -19,11 +19,11 @@ int	main( void )
 	float	floatB = 2.7182f;
 
 	std::cout <<"Comparing floats " << floatA << " and " << floatB << std::endl;
-	std::cout << "Max: " << max<float>(floatA, floatB) << std::endl;
-	std::cout << "Min: " << min<float>(floatA, floatB) << std::endl;
+	std::cout << "Max: " << ::max<float>(floatA, floatB) << std::endl;
+	std::cout << "Min: " << ::min<float>(floatA, floatB) << std::endl;
 	std::cout << "Performing swap" << std::endl;
 	std::cout << "Old values: " << floatA << ", " << floatB << std::endl;
-	swap<float>(floatA, floatB);
+	::swap<float>(floatA, floatB);
 	std::cout << "New values: " << floatA << ", " << floatB << std::endl;
 	std::cout << std::endl;
 
@@ -31,11 +31,11 @@ int	main( void )
 	long	longB = 2222L;
 
 	std::cout <<"Comparing longs " << longA << " and " << longB << std::endl;
-	std::cout << "Max: " << max<long>(longA, longB) << std::endl;
-	std::cout << "Min: " << min<long>(longA, longB) << std::endl;
+	std::cout << "Max: " << ::max<long>(longA, longB) << std::endl;
+	std::cout << "Min: " << ::min<long>(longA, longB) << std::endl;
 	std::cout << "Performing swap" << std::endl;
 	std::cout << "Old values: " << longA << ", " << longB << std::endl;
-	swap<long>(longA, longB);
+	::swap<long>(longA, longB);
 	std::cout << "New values: " << longA << ", " << longB << std::endl;
 	std::cout << std::endl;
 
@@ -43,11 +43,11 @@ int	main( void )
 	double	doubleB = 2.7182818284;
 
 	std::cout <<"Comparing doubles " << doubleA << " and " << doubleB << std::endl;
-	std::cout << "Max: " << max<double>(doubleA, doubleB) << std::endl;
-	std::cout << "Min: " << min<double>(doubleA, doubleB) << std::endl;
+	std::cout << "Max: " << ::max<double>(doubleA, doubleB) << std::endl;
+	std::cout << "Min: " << ::min<double>(doubleA, doubleB) << std::endl;
 	std::cout << "Performing swap" << std::endl;
 	std::cout << "Old values: " << doubleA << ", " << doubleB << std::endl;
-	swap<double>(doubleA, doubleB);
+	::swap<double>(doubleA, doubleB);
 	std::cout << "New values: " << doubleA << ", " << doubleB << std::endl;
 	std::cout << std::endl;
 
@@ -55,11 +55,11 @@ int	main( void )
 	char	charB = 'B';
 
 	std::cout <<"Comparing chars '" << charA << "' and '" << charB << "'" << std::endl;
-	std::cout << "Max: '" << max<char>(charA, charB) << "'" << std::endl;
-	std::cout << "Min: '" << min<char>(charA, charB) << "'" << std::endl;
+	std::cout << "Max: '" << ::max<char>(charA, charB) << "'" << std::endl;
+	std::cout << "Min: '" << ::min<char>(charA, charB) << "'" << std::endl;
 	std::cout << "Performing swap" << std::endl;
 	std::cout << "Old values: '" << charA << "', '" << charB << "'" << std::endl;
-	swap<char>(charA, charB);
+	::swap<char>(charA, charB);
 	std::cout << "New values: '" << charA << "', '" << charB << "'" << std::endl;
 	std::cout << std::endl;
 
@@ -67,7 +67,7 @@ int	main( void )
 	std::string stringB = "chaine2";
 
 	std::cout <<"Comparing std::strings \"" << stringA << "\" and \"" << stringB << "\"" << std::endl;
-	std::cout << "Max: \"" << ::max(stringA, stringB) << "\"" << std::endl;
+	std::cout << "Max: \"" << ::max<std::string>(stringA, stringB) << "\"" << std::endl;
 	std::cout << "Min: \"" << ::min(stringA, stringB) << "\"" << std::endl;
 	std::cout << "Performing swap" << std::endl;
 	std::cout << "Old values: \"" << stringA << "\", \"" << stringB << "\"" << std::endl;
@@ -81,13 +81,16 @@ int	main( void )
 	int	second = 0;
 	std::cout << "first = " << first << std::endl;
 	std::cout << "second = " << second << std::endl;
-	const int & returned = max(first, second);
-	std::cout << "returned = " << returned << std::endl;
+	const int & returned_max = ::max(first, second);
+	const int & returned_min = ::min(first, second);
+	std::cout << "returned_min = " << returned_min << std::endl;
+	std::cout << "returned_max = " << returned_max << std::endl;
 	first = 1;
 	second = 2;
 	std::cout << "first = " << first << std::endl;
 	std::cout << "second = " << second << std::endl;
-	std::cout << "returned = " << returned << std::endl;
+	std::cout << "returned_min = " << returned_min << std::endl;
+	std::cout << "returned_max = " << returned_max << std::endl;
 
 	return 0;
 }
