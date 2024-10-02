6031205fc16625bad5bc95559dde14277c099ad9
Albert <albertprpu97@gmail.com>
2024-10-02 18:29:00 +0200

    CPP_Module_09/ex00: New approach now using std::map

--- CPP_Module_09/ex00/BitcoinExchange.hpp
+++ CPP_Module_09/ex00/BitcoinExchange.hpp
@@ -4,16 +4,10 @@
 #include <algorithm>
 #include <iostream>
 #include <fstream>
-#include <vector>
-#include <list>
 #include <map>
-#include <deque>
-#include <stack>
-
 #include <exception>
-
-#include <sstream>
 #include <string>
+#include <sstream>
 
 class BitcoinExchange
 {
@@ -23,41 +17,17 @@ class BitcoinExchange
 		~BitcoinExchange();
 		BitcoinExchange &operator=( BitcoinExchange const &other );
 
-		BitcoinExchange	operator+( BitcoinExchange const & rhs );
-		BitcoinExchange	operator-( BitcoinExchange const & rhs );
-		BitcoinExchange	operator*( BitcoinExchange const & rhs );
-		BitcoinExchange	operator/( BitcoinExchange const & rhs );
-
-		void	loadFile( std::string & filename );
-		void	parseFile( std::ifstream & file );
-		std::vector<std::pair<std::string, float> >	getVectorFromFile( std::ifstream & file, const std::string divider );
-
-		void	verifyDate( std::string & date );
-		long	getDate( std::string & date );
-		float	getValue( std::string & value );
-
-		void	setDataVector( void );
-
-		void	checkConversion( std::string & filename );
-
-	class CouldNotOpenFileException : public std::exception
-	{
-		public:
-			virtual const char *what() const throw();
-	};
-	class BadInputException : public std::exception
-	{
-		public:
-			virtual const char *what() const throw();
-	};
-	class DatePreviousToDataException : public std::exception
-	{
-		public:
-			virtual const char *what() const throw();
-	};
-
 	private:
-		std::vector<std::pair<std::string, float> >	_data;
+		void	initDataMap( void );
+
+		std::map<std::string, float>	_data;
+
+	public:
+		class OpenFileException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
 
 };
 
---
+++ CPP_Module_09/ex00/BitcoinExchangeOld.hpp
@@ -0,0 +1,68 @@
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <list>
+#include <map>
+#include <deque>
+#include <stack>
+
+#include <exception>
+
+#include <sstream>
+#include <string>
+
+class BitcoinExchange
+{
+	public:
+		BitcoinExchange( void );
+		BitcoinExchange( BitcoinExchange const &other );
+		~BitcoinExchange();
+		BitcoinExchange &operator=( BitcoinExchange const &other );
+
+		BitcoinExchange	operator+( BitcoinExchange const & rhs );
+		BitcoinExchange	operator-( BitcoinExchange const & rhs );
+		BitcoinExchange	operator*( BitcoinExchange const & rhs );
+		BitcoinExchange	operator/( BitcoinExchange const & rhs );
+
+		void	loadFile( std::string & filename );
+		void	parseFile( std::ifstream & file );
+
+		std::vector<std::pair<std::string, float> >	getVectorFromFile( std::ifstream & file, const std::string divider );
+		std::vector<std::string>	getVectorFromFile2( std::ifstream & file );
+
+		void	verifyDate( std::string & date );
+		long	getDate( std::string & date );
+		float	getValue( std::string & value );
+
+		void	setDataVector( void );
+
+		void	checkConversion( std::string & filename );
+
+	class CouldNotOpenFileException : public std::exception
+	{
+		public:
+			virtual const char *what() const throw();
+	};
+	class BadInputException : public std::exception
+	{
+		public:
+			virtual const char *what() const throw();
+	};
+	class DatePreviousToDataException : public std::exception
+	{
+		public:
+			virtual const char *what() const throw();
+	};
+
+	private:
+		std::vector<std::pair<std::string, float> >	_data2;
+		std::vector<std::string>	_data3;
+		std::map<std::string, float>	_data;
+
+};
+
+#endif // BITCOINEXCHANGE_HPP
