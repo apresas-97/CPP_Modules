d13efcc7adf31385d6378ea27904366dcd3f8b41
apresas- <albertprpu97@gmail.com>
2024-10-10 20:15:50 +0200

    CPP_Module_09/ex02: Added a note and made a few changes in test.cpp

--- CPP_Module_09/ex02/test.cpp
+++ CPP_Module_09/ex02/test.cpp
@@ -32,6 +32,25 @@ size_t	getNextJacobsthalNumber( void )
 	return tmp;
 }
 
+size_t	jacobsthalNumber( size_t k )
+{
+	if (k == 0)
+		return 0;
+	if (k == 1)
+		return 1;
+
+	size_t	a = 0;
+	size_t	b = 1;
+	size_t	c;
+	for (size_t i = 2; i <= k; i++)
+	{
+		c = 2 * b + a;
+		a = b;
+		b = c;
+	}
+	return b;
+}
+
 void	binaryInsert( std::vector<int> & vec, int value )
 {
 	size_t	i = vec.size() - 2;
@@ -51,6 +70,36 @@ void	binaryInsert( std::vector<int> & vec, int value )
 	}
 }
 
+// void	binaryInsert( std::vector<std::pair<int, int> > & vec, int value )
+// {
+// 	size_t	i = vec.size() - 2;
+// 	if (value >= vec[i].first)
+// 	{
+// 		if (value >= vec[i + 1].first)
+// 			vec.push_back(std::make_pair(value, 0));
+// 		else // value < vec[i + 1].first
+// 			vec.insert(vec.begin() + (i + 1), std::make_pair(value, 0));
+// 	}
+// 	else // value < vec[i].first
+// 	{
+// 		if (value >= vec[i - 1].first)
+// 			vec.insert(vec.begin() + i, std::make_pair(value, 0));
+
+// 		else // value < vec[i - 1].first
+// 			vec.insert(vec.begin() + (i - 1), std::make_pair(value, 0));
+// 	}
+// }
+
+int	getLastValue( std::vector<int>::iterator lastValueIt )
+{
+	return *lastValueIt;
+}
+
+int	getLastValue( std::vector<std::pair<int, int> >::iterator lastValueIt )
+{
+	return lastValueIt->first;
+}
+
 // mergeInsertionSort, called with iterators
 template<typename Iterator>
 void	mergeInsertionSortVector( Iterator begin, Iterator end )
@@ -64,7 +113,14 @@ void	mergeInsertionSortVector( Iterator begin, Iterator end )
 	if (size < 2) // Maybe?
 		return ;
 
-
+	Iterator endTrue = end;
+	int	lastValue = 0;
+	if (size % 2 != 0)
+	{
+		lastValue = getLastValue(end - 1);
+		std::cout << "Last value = " << lastValue << std::endl;
+		end--;
+	}
 	// Maybe have a bool has_stray if size is odd, then
 	// redefine end to be end - 1, but keep track of the original end
 
@@ -94,37 +150,109 @@ void	mergeInsertionSortVector( Iterator begin, Iterator end )
 
 
 	std::vector<int> vecChain;
+	std::vector<int>	pend;
 	for (size_t i = 0; i < mainChain.size(); i++)
+	{
 		vecChain.push_back(mainChain[i].first);
+		pend.push_back(mainChain[i].second);
+	}
+	// if (size % 2 != 0)
+	// 	pend.push_back(lastValue);
+	
+	std::cout << "MainChain:" << std::endl;
+	printVector(vecChain.begin(), vecChain.end());
+	std::cout << "Pend:" << std::endl;
+	printVector(pend.begin(), pend.end());
+	std::cout << std::endl;
+
 
 	// insert b1
 	std::cout << "Inserting b1" << std::endl;
 	
-	size_t	b_end = size - vecChain.size() - 1;
-	size_t	i = b_end - getNextJacobsthalNumber();
-	size_t	auxNextJacobsthalNumber = getNextJacobsthalNumber();
-	std::cout << "b_end = " << b_end << std::endl;
-	std::cout << "nextJacobsthalNumber = " << auxNextJacobsthalNumber << std::endl;
-	vecChain.push_back(mainChain[i].second); // insert b1
-	printVector(vecChain.begin(), vecChain.end());
-	while (i >= 0)
+	// size_t	b_end = size - vecChain.size() - 1;
+	// size_t	i = b_end - getNextJacobsthalNumber();
+	// size_t	auxNextJacobsthalNumber = getNextJacobsthalNumber();
+	// std::cout << "b_end = " << b_end << std::endl;
+	// std::cout << "nextJacobsthalNumber = " << auxNextJacobsthalNumber << std::endl;
+	// vecChain.push_back(mainChain[i].second); // insert b1
+	// printVector(vecChain.begin(), vecChain.end());
+	// while (i >= 0)
+	// {
+	// 	std::cout << "Starting loop" << std::endl;
+	// 	// std::cout << "Check" << std::endl;
+	// 	std::cout << "i = " << i << std::endl;
+	// 	std::cout << "b_end = " << b_end << std::endl;
+	// 	size_t	i = b_end - getNextJacobsthalNumber();
+	// 	size_t	next_end = 0;
+	// 	while (i != b_end)
+	// 	{
+	// 		binaryInsert(vecChain, mainChain[i].second);
+	// 		next_end++;
+	// 		i++;
+	// 	}
+	// 	std::cout << "next_end = " << getNextJacobsthalNumber() << std::endl;
+	// 	b_end -= next_end;
+	// 	std::cout << "loop!" << std::endl;
+	// }
+
+
+	///
+
+	//
+	size_t	stopper = 0;
+
+	size_t	k = 0;
+	size_t	pend_size = pend.size();
+	while (true)
 	{
-		std::cout << "Starting loop" << std::endl;
-		// std::cout << "Check" << std::endl;
+		if (jacobsthalNumber(k) >= pend_size)
+			break ;
+		size_t	i = pend_size - jacobsthalNumber(k) - 1;
 		std::cout << "i = " << i << std::endl;
-		std::cout << "b_end = " << b_end << std::endl;
-		size_t	i = b_end - getNextJacobsthalNumber();
-		size_t	next_end = 0;
-		while (i != b_end)
+		size_t	j = 0;
+		while (i < pend_size)
 		{
-			binaryInsert(vecChain, mainChain[i].second);
-			next_end++;
+			std::cout << "i = " << i << std::endl;
+			std::cout << "pend_size = " << pend_size << std::endl;
+
+			std::cout << "Inserting " << pend[i] << std::endl;
+			binaryInsert(vecChain, pend[i]);
+			std::cout << "vecChain:" << std::endl;
+			printVector(vecChain.begin(), vecChain.end());
+			std::cout << "pend:" << std::endl;
+			printVector(pend.begin(), pend.end());
+			std::cout << std::endl;
 			i++;
+			j++;
 		}
-		std::cout << "next_end = " << getNextJacobsthalNumber() << std::endl;
-		b_end -= next_end;
-		std::cout << "loop!" << std::endl;
+
+		if (i == 0)
+			break ;
+		if (stopper >= 10)
+			break ;
+		stopper++;
+		pend_size -= j;
+		std::cout << "New pend_size = " << pend_size << std::endl;
+		k++;
 	}
+
+	// Insert the last value if it exists
+	if (size % 2 != 0)
+	{
+		std::cout << "Size = " << size << std::endl;
+		std::cout << "Inserting the odd value" << std::endl;
+		binaryInsert(vecChain, lastValue);
+	}
+
+	std::cout << "vecChain:" << std::endl;
+	printVector(vecChain.begin(), vecChain.end());
+	std::cout << "pend:" << std::endl;
+	printVector(pend.begin(), pend.end());
+	std::cout << std::endl;
+
+	// FIRST ITERATION ACTUALLY WORKS, BUT CHANGES ARE NOT
+	// REFLECTED IN THE ORIGINAL VECTOR OF COURSE
+	// SO I NEED TO ADAPT IT
 
 }
 
